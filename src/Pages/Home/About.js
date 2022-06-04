14d40c8bbc919d2d8c6a23450ea929dd2a80b83b
imranahmed1990 <imranmarin1990@gmail.com>
2022-06-04 22:37:10 +0600

    added

--- src/Pages/Home/About.js
+++ src/Pages/Home/About.js
@@ -8,10 +8,11 @@ const About = () => {
             <div class="card lg:card-side bg-base-100 shadow-xl w-4/5 mx-auto my-60 bg-warning">
                 <img width={600} src={img} alt="Movie" className='bg-dark' />
                 <div class="card-body bg-warning p-20">
-                    <h2 class="card-title text-4xl font-bold">Font End Developer</h2>
-                    <p className='pt-5'>I'm web designer, and I'm very passionate and dedicated to my work. With 20 years experience as a professional web developer, I have acquired the skills and knowledge necessary to make your project a success. I enjoy every step of the design process, from discussion and collaboration.</p>
+                    <h2 className=' font-bold'>ABOUT</h2>
+                    <h2 class="card-title text-5xl font-bold">Font End Developer</h2>
+                    <p className='pt-5'>I'm web developer, and I'm very passionate and dedicated to my work. With 1 year experience as a professional web developer, I have acquired the skills and knowledge necessary to make your project a success. I enjoy every step of the design process, from discussion and collaboration.</p>
                     <div class="card-actions justify-start">
-                        <button class="btn btn-outline">Download CV</button>
+                        <button class="btn btn-outline ">Download CV</button>
                     </div>
                 </div>
             </div>
